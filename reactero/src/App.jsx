f8f79b5392001f40d4e1ee9f339df58028f01f44
PEDRO VICTOR RODRIGUES DE ALMEIDA <24122478@dsn127m1246727.cfp127aluno.local>
2025-09-23 17:25:16 -0300

    react inicio

---
+++ reactero/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NovaSenha from './pages/NovaSenha/Novasenha'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <NovaSenha />
+    </div>
+  )
+}
+
+export default App
---
+++ reactero/src/pages/NovaSenha/Novasenha.jsx
@@ -0,0 +1,122 @@
+import { useState } from 'react';
+import './NovaSenha.css';
+import Fundo from '../../assets/fundo.png'
+import logo from '../../assets/logo.png'
+// Importe suas imagens/ícones aqui (ajuste os caminhos)
+// import logo from './caminho/da/logo.png';
+// import emailIcon from './caminho/email-icon.svg';
+// import passwordIcon from './caminho/password-icon.svg';
+
+const NovaSenha = () => {
+  const [formData, setFormData] = useState({
+    novaSenha: '',
+    confirmarSenha: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prev => ({
+      ...prev,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Dados do formulário:', formData);
+  };
+
+  return (
+    <div className="nova-senha-container">
+         <img src={Fundo} id='fundo' alt="" />
+      <div className="nova-senha-card">
+        
+        {/* DIV PARA A LOGO */}
+        <div className="logo-container">
+          <img 
+            src= {logo} id='logo'
+            alt="Logo" 
+            className="logo"
+            // ou use import: src={logo}
+          />
+        </div>
+
+        <h1 className="nova-senha-title">Criar uma nova senha</h1>
+        
+        <form onSubmit={handleSubmit} className="nova-senha-form">
+          
+          {/* CAMPO 1 COM ÍCONE */}
+          <div className="input-group">
+            <label htmlFor="novaSenha" className="input-label">
+              A Nova Senha
+            </label>
+            <div className="input-with-icon">
+              <div className="icon-container">
+                <img 
+                  src="../assets/react.svg" 
+                  alt="Senha" 
+                  className="input-icon"
+                />
+              </div>
+              <input
+                id="novaSenha"
+                name="novaSenha"
+                type="password"
+                value={formData.novaSenha}
+                onChange={handleChange}
+                className="password-input"
+                placeholder="Digite a nova senha"
+              />
+            </div>
+          </div>
+          
+          {/* CAMPO 2 COM ÍCONE */}
+          <div className="input-group">
+            <label htmlFor="confirmarSenha" className="input-label">
+              A Confirmar Senha
+            </label>
+            <div className="input-with-icon">
+              <div className="icon-container">
+                <img 
+                  src="/caminho/para/confirm-icon.png" 
+                  alt="Confirmar" 
+                  className="input-icon"
+                />
+              </div>
+              <input
+                id="confirmarSenha"
+                name="confirmarSenha"
+                type="password"
+                value={formData.confirmarSenha}
+                onChange={handleChange}
+                className="password-input"
+                placeholder="Confirme a senha"
+              />
+            </div>
+          </div>
+          
+          <button type="submit" className="reset-button">
+            Redefinir Senha
+          </button>
+        </form>
+
+        {/* DIV EXTRA PARA OUTROS ÍCONES (opcional) */}
+        <div className="additional-icons">
+          <img 
+            src="/caminho/para/icon1.png" 
+            alt="Ícone 1" 
+            className="additional-icon"
+          />
+          <img 
+            src="/caminho/para/icon2.png" 
+            alt="Ícone 2" 
+            className="additional-icon"
+          />
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default NovaSenha;

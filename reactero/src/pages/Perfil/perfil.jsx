b79e9aca9528f53b1c89ea17a7258cff1476d20d
PEDRO VICTOR RODRIGUES DE ALMEIDA <24122478@CFP127ALUNO.LOCAL>
2025-11-06 08:53:41 -0300

    nav bar tela perfil

--- reactero/src/pages/Perfil/perfil.jsx
+++ reactero/src/pages/Perfil/perfil.jsx
@@ -139,25 +139,7 @@ function Perfil() {
           <Link to="/home" className="btn">Home</Link>
           <Link to="/perfil" className="btn">Perfil</Link>
           <Link to="/sobre" className="btn">Sobre</Link>
-          <svg
-            className="outline"
-            overflow="visible"
-            width="400"
-            height="60"
-            viewBox="0 0 400 60"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect
-              className="rect"
-              pathLength="100"
-              x="0"
-              y="0"
-              width="400"
-              height="60"
-              fill="transparent"
-              strokeWidth="5"
-            ></rect>
-          </svg>
+          
         </div>
       </div>
 
